4dd2eb8b1cfd88aa26b0bd88a7fa033a1d4b28b7
UPUSERDEV33 <54555776+UPUSERDEV33@users.noreply.github.com>
2019-09-25 21:33:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -464,6 +464,172 @@ if (message.content.startsWith(prefix + 'perms')) {
 
     }
 });
+
+
+//var       
+
+var copy = "ALPHACODE STEWART"
+
+
+var mo = ""
+var po = ""
+var lev = ""
+
+
+bot.on("ready", async () => { //      
+	
+	    console.log(`I'm Online \n By ${copy}`) //    
+	    console.log(`Logged in as ${bot.user.tag}!`); //   
+	
+	    bot.user.setGame(`${bot.users.size} users `,"http://twitch.tv/") 
+	    bot.user.setStatus("online")
+	
+}); //  
+
+
+
+client.on('message', message => {
+	
+	if (message.content.startsWith("*profile")) { // 
+		 let canvas = new Canvas(300, 300) //   
+		  let ctx = canvas.getContext('2d')
+		      let Image = Canvas.Image
+		          
+		         
+		                            //  ava.src = buf;
+		      
+		          fs.readFile(__dirname + '/images_profile/profile.png', function(err, picture) { //  
+				        if (err) throw err
+				        var img = new Image
+					        		var url = message.author.avatarURL; // 
+				  		url = url.substring(0, url.indexOf('?'));
+				  
+				  		r1.get(url).then(res => {
+										var dataURL = res.body.toString('base64');
+										dataURL = 'data:image/png;base64,' + dataURL;
+										img.onload = function() {
+											
+															ctx.save();
+											    		ctx.beginPath();
+											    		ctx.arc(54, 103, 47, 0, Math.PI * 2, true); //  
+													    ctx.closePath();
+													    ctx.clip();
+													    ctx.drawImage(img, 8, 57, 92, 92); // 
+													    ctx.restore();
+										}
+										img.src = dataURL;
+						});
+				  		
+				        img.onload = () => {
+						        ctx.drawImage(img, 1, 1, 300, 300)
+						     //   ctx.drawImage(message.author.avatarURL, 152, 27, 95, 95);
+						        ctx.font = "regular 11px Cairo" //   
+						        ctx.fillStyle = "#9f9f9f" //  
+						        ctx.fillText(`${message.author.username}`, 140, 137)
+						        ctx.fillText(`${mo}  `, 143, 219) //money
+						        ctx.fillText(`${po}`, 120, 202) // 
+						
+						        //Level
+						        ctx.font = "regular 21px Cairo"
+						        ctx.fillStyle = "#ffffff"
+						        ctx.fillText(`${lev}`, 47, 255) //
+						
+						        ctx.save()
+						        
+					}
+					      img.src = picture
+				  			
+			  })
+				
+		   
+		
+		    
+		
+		    setTimeout(function() {
+			          fs.readFile(__dirname + '/images_profile/diamond_prof_bg.png', function(err, picture) {
+					          if (err) throw err
+					          var img = new Image
+						          img.onload = () => {
+								            ctx.drawImage(img, -1, -1, 0, 0)
+							  }
+							          img.src = picture
+					          let inventoryPicture = canvas.toDataURL()
+						          let data = inventoryPicture.replace(/^data:image\/\w+;base64,/, "")
+							          let buf = new Buffer(data, 'base64')
+								        fs.writeFile(`image.png`, buf)
+					        
+					          message.channel.send("", {
+							            file: `image.png` 
+						  })
+				  })
+		    }, 1000)
+		
+		
+		    function roundedImage(x, y, width, height, radius) {
+			          ctx.beginPath();
+			          ctx.moveTo(x + radius, y);
+			          ctx.lineTo(x + width - radius, y);
+			          ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+			          ctx.lineTo(x + width, y + height - radius);
+			          ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+			          ctx.lineTo(x + radius, y + height);
+			          ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+			          ctx.lineTo(x, y + radius);
+			          ctx.quadraticCurveTo(x, y, x + radius, y);
+			          ctx.closePath();
+		    }
+		
+		    function wrapText(context, text, x, y, maxWidth, lineHeight) {
+			    
+			          var words = text.split(' '),
+				              line = '',
+				              lineCount = 0,
+				              i,
+				              test,
+				              metrics;
+			    
+			          for (i = 0; i < words.length; i++) {
+					          test = words[i];
+					          metrics = context.measureText(test);
+					          while (metrics.width > maxWidth) {
+							  
+							            test = test.substring(0, test.length - 1);
+							            metrics = context.measureText(test);
+						  }
+					          if (words[i] != test) {
+							            words.splice(i + 1, 0, words[i].substr(test.length))
+							            words[i] = test;
+						  }
+					  
+					          test = line + words[i] + ' ';
+					          metrics = context.measureText(test);
+					  
+					          if (metrics.width > maxWidth && i > 0) {
+							            context.fillText(line, x, y);
+							            line = words[i] + ' ';
+							            y += lineHeight;
+							            lineCount++;
+						  } else {
+							            line = test;
+						  }
+				  }
+			    
+			          ctx.fillText(line, x, y);
+		    }
+		  
+		
+		
+		
+	};
+	
+	
+	
+	
+});
+
+
+
+
  client.on('message', message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
